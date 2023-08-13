d996fd1b837400a53819a87c24489e288b08a9c1
FarruxMustafo <FarruxMustafo1@gmail.com>
2023-08-13 22:16:33 +0500

    splash page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,27 +3,43 @@
 import 'package:flutter/material.dart';
 import 'package:restarant/consts/consts.dart';
 
-class HomeScreen extends StatefulWidget {  
+
+class  HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  @override  
-  Widget build(BuildContext context) {  
-    return Scaffold(  
-      backgroundColor: color,
-      appBar: AppBar(title:Text("Splash Screen Example")),  
-      body: Center(  
-          child:Stack(
-            children: [
-              Image.asset("assets/splaw1.png"),
-           const    Text("Welcome to Home Page",  
-                style: TextStyle( color: Colors.white, fontSize: 30)  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+          child: Row(
+              children: [
+          NavigationRail(
+            
+            labelType: NavigationRailLabelType.all,
+            selectedIndex: 0,
+            selectedLabelTextStyle: const TextStyle(color: Colors.white, fontSize:20),
+            unselectedLabelTextStyle: TextStyle(color: Colors.white30, fontSize: 15),
+            backgroundColor: color,
+            destinations: const [
+              NavigationRailDestination(
+                
+                icon:SizedBox(),
+                  label: RotatedBox(quarterTurns: -1, child: Text("Блюда"))),
+              NavigationRailDestination(
+                  icon:SizedBox(),
+                  label: RotatedBox(quarterTurns: -1, child: Text("Блюда"))),
+              NavigationRailDestination(
+                 icon:SizedBox(),
+                  label: RotatedBox(quarterTurns: -1, child: Text("Блюда"))),
+            ],
+          )
+              ],
             ),
-            ]
-          )  
-      ),  
-    );  
-  }  
-}   
+        ));
+  }
+}
